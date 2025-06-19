c74231b704f0dc1cbf822216bf178ca0a3b7f47d
Muhammadsaid-Buxoriy <seitmuradovmuhammadsaid@gmail.com>
2025-06-19 17:39:21 +0500

    eski kod

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./components/context/AuthContext.jsx";
-import { ThemeProvider } from "./components/context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
